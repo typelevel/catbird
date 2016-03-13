d7735b1c4f106d9bfb8412e74fd3201e5d6d4231
Travis Brown <travisrobertbrown@gmail.com>
2016-03-13 11:56:35 -0500

    Add benchmark for Rerunnable

---
+++ benchmark/src/test/scala/io/catbird/benchmark/RerunnableBenchmarkSpec.scala
@@ -0,0 +1,27 @@
+package io.catbird.benchmark
+
+import org.scalatest.{ BeforeAndAfter, FlatSpec }
+
+class RerunnableBenchmarkSpec extends FlatSpec with BeforeAndAfter {
+  val benchmark: RerunnableBenchmark = new RerunnableBenchmark
+  val sum = benchmark.numbers.sum
+
+  before { benchmark.initPool() }
+  after { benchmark.shutdownPool() }
+
+  "The benchmark" should "correctly calculate the sum using futures" in {
+    assert(benchmark.sumIntsF === sum)
+  }
+
+  it should "correctly calculate the sum using futures and future pools" in {
+    assert(benchmark.sumIntsPF === sum)
+  }
+
+  it should "correctly calculate the sum using rerunnables" in {
+    assert(benchmark.sumIntsR === sum)
+  }
+
+  it should "correctly calculate the sum using rerunnables and future pools" in {
+    assert(benchmark.sumIntsPR === sum)
+  }
+}
