1969ae343195eb935a2070fb0c1cf6389197a8ad
Travis Brown <travisrobertbrown@gmail.com>
2017-01-13 11:35:29 -0600

    Clean-up unused imports

--- finagle/src/test/scala/io/catbird/finagle/service.scala
+++ finagle/src/test/scala/io/catbird/finagle/service.scala
@@ -1,17 +1,13 @@
 package io.catbird.finagle
 
 import cats.Eq
-import cats.data.Kleisli
 import cats.instances.int._
 import cats.laws.discipline._
 import cats.laws.discipline.eq._
 import com.twitter.conversions.time._
 import com.twitter.finagle.Service
-import com.twitter.util.Future
-import io.catbird.util._
 import org.scalatest.FunSuite
 import org.typelevel.discipline.scalatest.Discipline
-import scala.util.{ Failure, Success }
 
 class ServiceSuite extends FunSuite with Discipline with
   ServiceInstances with ArbitraryInstances with EqInstances {
