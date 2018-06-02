92bd54fc9c823d5ccf8af0159c663852f13a4583
Ben Spencer <ben.spencer@healthforge.io>
2018-06-02 14:47:12 +0100

    Add generic futureToAsync and define rerunnableToIO in terms of it.

--- effect/src/main/scala/io/catbird/util/effect/package.scala
+++ effect/src/main/scala/io/catbird/util/effect/package.scala
@@ -1,15 +1,17 @@
 package io.catbird.util
 
-import cats.effect.IO
-import com.twitter.util.{ Return, Throw }
+import cats.effect.{ Async, IO }
+import com.twitter.util.{ Future, Return, Throw }
 import java.lang.Throwable
 import scala.util.{ Left, Right }
 
 package object effect extends RerunnableInstances {
-  final def rerunnableToIO[A](fa: Rerunnable[A]): IO[A] = IO.async { k =>
-    fa.run.respond {
+  def futureToAsync[F[_], A](fa: => Future[A])(implicit F: Async[F]): F[A] = F.async { k =>
+    fa.respond {
       case Return(a) => k(Right[Throwable, A](a))
       case Throw(err) => k(Left[Throwable, A](err))
     }
   }
+
+  final def rerunnableToIO[A](fa: Rerunnable[A]): IO[A] = futureToAsync[IO, A](fa.run)
 }
