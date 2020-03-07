36e241aba638a53b13b09f08ec48cc10b4b52223
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2020-03-07 17:51:07 +0100

    Add effect conversion functions which shift correctly

--- effect/src/main/scala/io/catbird/util/effect/package.scala
+++ effect/src/main/scala/io/catbird/util/effect/package.scala
@@ -1,11 +1,16 @@
 package io.catbird.util
 
-import cats.effect.{ Async, IO }
+import cats.effect.{ Async, ContextShift, IO }
 import com.twitter.util.{ Future, Return, Throw }
 import java.lang.Throwable
+
 import scala.util.{ Left, Right }
 
 package object effect extends RerunnableInstances {
+
+  /**
+   * Converts the `Future` to `F` without changing the underlying execution (same thread pool!).
+   */
   def futureToAsync[F[_], A](fa: => Future[A])(implicit F: Async[F]): F[A] = F.async { k =>
     fa.respond {
       case Return(a)  => k(Right[Throwable, A](a))
@@ -13,5 +18,27 @@ package object effect extends RerunnableInstances {
     }
   }
 
-  final def rerunnableToIO[A](fa: Rerunnable[A]): IO[A] = futureToAsync[IO, A](fa.run)
+  /**
+   * The same as `futureToAsync` but doesn't stay on the thread pool of the `Future` and instead shifts execution
+   * back to the one provided by `ContextShift[F]` (which is usually the default one).
+   *
+   * This is likely what you want when you interact with libraries that return a `Future` like `finagle-http` where
+   * the `Future` is running on a thread pool controlled by the library (e.g. the underlying Netty pool).
+   * It also is closer to the behavior of `IO.fromFuture` for Scala futures which also shifts back.
+   */
+  def futureToAsyncAndShift[F[_], A](fa: => Future[A])(implicit F: Async[F], CS: ContextShift[F]): F[A] =
+    F.guarantee(futureToAsync[F, A](fa))(CS.shift)
+
+  /**
+   * Converts the `Rerunnable` to `F` without changing the underlying execution (same thread pool!).
+   */
+  final def rerunnableToIO[A](fa: Rerunnable[A]): IO[A] =
+    futureToAsync[IO, A](fa.run)
+
+  /**
+   * The same as `rerunnableToIO` but doesn't stay on the thread pool of the `Rerunnable` and instead shifts execution
+   * back to the one provided by `ContextShift[F]` (which is usually the default one).
+   */
+  final def rerunnableToIOAndShift[A](fa: Rerunnable[A])(implicit CS: ContextShift[IO]): IO[A] =
+    futureToAsyncAndShift[IO, A](fa.run)
 }
