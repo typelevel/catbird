b03db49abba97ca4a10f7157000678abbaaf276b
Ben Spencer <dangerous.ben@gmail.com>
2020-07-12 18:40:21 +0100

    Add Alternative/MonoidK instance for AsyncStream.

--- util/src/main/scala/io/catbird/util/asyncstream.scala
+++ util/src/main/scala/io/catbird/util/asyncstream.scala
@@ -1,16 +1,19 @@
 package io.catbird
 package util
 
-import cats.{ Eq, Monad, Monoid, Semigroup, StackSafeMonad }
+import cats.{ Alternative, Eq, Monad, Monoid, Semigroup, StackSafeMonad }
 import com.twitter.concurrent._
 import com.twitter.util._
 
 trait AsyncStreamInstances extends AsyncStreamInstances1 {
-  implicit final val asyncStreamInstances: Monad[AsyncStream] =
-    new StackSafeMonad[AsyncStream] {
+  implicit final val asyncStreamInstances: Monad[AsyncStream] with Alternative[AsyncStream] =
+    new StackSafeMonad[AsyncStream] with Alternative[AsyncStream] {
       final def pure[A](a: A): AsyncStream[A] = AsyncStream.of(a)
       final def flatMap[A, B](fa: AsyncStream[A])(f: A => AsyncStream[B]): AsyncStream[B] = fa.flatMap(f)
       override final def map[A, B](fa: AsyncStream[A])(f: A => B): AsyncStream[B] = fa.map(f)
+
+      def empty[A]: AsyncStream[A] = AsyncStream.empty[A]
+      def combineK[A](x: AsyncStream[A], y: AsyncStream[A]): AsyncStream[A] = x ++ y
     }
 
   implicit final def asyncStreamSemigroup[A](implicit A: Semigroup[A]): Semigroup[AsyncStream[A]] =
--- util/src/test/scala/io/catbird/util/asyncstream.scala
+++ util/src/test/scala/io/catbird/util/asyncstream.scala
@@ -14,6 +14,7 @@ class AsyncStreamSuite extends CatbirdSuite with AsyncStreamInstances with Arbit
   implicit val eqAsyncStreamAsyncStreamInt: Eq[AsyncStream[AsyncStream[Int]]] = asyncStreamEq(1.second)
   implicit val eqAsyncStreamIntIntInt: Eq[AsyncStream[(Int, Int, Int)]] = asyncStreamEq[(Int, Int, Int)](1.second)
 
+  checkAll("AsyncStream[Int]", AlternativeTests[AsyncStream].alternative[Int, Int, Int])
   checkAll("AsyncStream[Int]", MonadTests[AsyncStream].monad[Int, Int, Int])
   checkAll("AsyncStream[Int]", SemigroupTests[AsyncStream[Int]](asyncStreamSemigroup[Int]).semigroup)
   checkAll("AsyncStream[Int]", MonoidTests[AsyncStream[Int]].monoid)
