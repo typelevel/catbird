277bcd9cb9200c5a582a7983c732c9361386261d
Ben Spencer <dangerous.ben@gmail.com>
2021-05-23 15:35:40 +0100

    CE3: Install custom monitor for law testing suites.
    Cuts down on noise from the root monitor.

--- effect3/src/test/scala/io/catbird/util/effect/BaseLawSuite.scala
+++ effect3/src/test/scala/io/catbird/util/effect/BaseLawSuite.scala
@@ -1,6 +1,7 @@
 package io.catbird.util
 package effect
 
+import com.twitter.util.Monitor
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.prop.Configuration
 import org.typelevel.discipline.scalatest.FunSuiteDiscipline
@@ -10,4 +11,9 @@ abstract class BaseLawSuite
     with FunSuiteDiscipline
     with Configuration
     with ArbitraryInstances
-    with EqInstances
+    with EqInstances {
+  protected val monitor = Monitor.mk { case e => println("Monitored: " + e); true }
+
+  override protected def withFixture(test: NoArgTest) =
+    Monitor.using(monitor)(test())
+}
