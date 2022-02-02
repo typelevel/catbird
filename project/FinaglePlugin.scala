3d50050a3f24cad8503d75933517637043fd186b
Arman Bilge <armanbilge@gmail.com>
2022-02-02 02:36:37 +0000

    Create root aggregate

--- project/FinaglePlugin.scala
+++ project/FinaglePlugin.scala
@@ -14,18 +14,26 @@ object FinaglePlugin extends AutoPlugin {
     "com.twitter" %% "finagle-core" % versions.head
   )
 
-  override lazy val extraProjects = modules.map { module =>
-    Project(module.name, file(s".${module.name}"))
+  override lazy val extraProjects = {
+    val subprojects = modules.map { module =>
+      Project(module.name, file(s".${module.name}"))
+        .enablePlugins(NoPublishPlugin)
+        .settings(
+          libraryDependencies += module,
+          mimaCurrentClassfiles := {
+            (Compile / dependencyClasspath).value.seq.map(_.data).find(_.getName.startsWith(module.name)).get
+          },
+          mimaPreviousArtifacts := versions.tail.map { v =>
+            module.withRevision(v)
+          }.toSet
+        )
+    }
+
+    val rootFinagle = project.in(file(s".rootFinagle"))
       .enablePlugins(NoPublishPlugin)
-      .settings(
-        libraryDependencies += module,
-        mimaCurrentClassfiles := {
-          (Compile / dependencyClasspath).value.seq.map(_.data).find(_.getName.startsWith(module.name)).get
-        },
-        mimaPreviousArtifacts := versions.tail.map { v =>
-          module.withRevision(v)
-        }.toSet
-      )
+      .aggregate(subprojects.map(_.project): _*)
+
+    rootFinagle +: subprojects
   }
 
 }
