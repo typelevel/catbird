976e505086041a5bce85b7bc27056e17d751b6e6
Brian Holt <bholt@dwolla.com>
2022-10-19 22:57:15 -0500

    update Finagle version to 22.7.0

--- project/FinaglePlugin.scala
+++ project/FinaglePlugin.scala
@@ -12,7 +12,15 @@ object FinaglePlugin extends AutoPlugin {
     lazy val finagleVersion = versions.head
   }
 
-  val versions = Seq("21.8.0", "21.6.0")
+  /* When a new Finagle version is released, add it to the beginning of
+   * this list. If `rootFinagle/mimaReportBinaryIssues` shows no issues,
+   * we have a chain of binary-compatible releases and the new list can
+   * be left as it is (i.e. with the new version at the head). If issues
+   * are found, then remove everything other than the new version,
+   * because the newest release is not binary compatible with the older
+   * versions it was checked against.
+   */
+  val versions = Seq("22.7.0")
 
   lazy val modules = Seq(
     "com.twitter" %% "finagle-core" % versions.head
